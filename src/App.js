ccb4557540f26253b9b585d5e265f79952ecccc1
PinsaraK <pinsarakuruppu@gmail.com>
2021-05-13 02:15:43 -0400

    final commit

--- src/App.js
+++ src/App.js
@@ -1,22 +1,54 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import React, { useState, useEffect } from "react";
+import Amplify, { API, graphqlOperation } from "aws-amplify";
+import awsExports from "./aws-exports";
+
+import { updateDoge } from "./graphql/mutations";
+import { getDoge } from "./graphql/queries";
+import "./App.css";
+Amplify.configure(awsExports);
 
 function App() {
+  const [dogePrice, setDogePrice] = useState(0);
+
+  const fetchDogePrice = async () => {
+    try {
+      const dogeData = await API.graphql(graphqlOperation(getDoge));
+      const dogePrice = dogeData.data.getDoge.price;
+      setDogePrice(dogePrice);
+    } catch (e) {
+      console.log("error fetching dogePrice");
+      console.log(e);
+    }
+  };
+
+  useEffect(() => {
+    fetchDogePrice();
+  }, []);
+
+  const updateDogePrice = async () => {
+    try {
+      const dogeData = await API.graphql(graphqlOperation(getDoge));
+      const dogePrice = dogeData.data.getDoge.price + 0.1;
+
+      const updatedDogePrice = await API.graphql(
+        graphqlOperation(updateDoge, { input: dogePrice })
+      );
+      console.log(updatedDogePrice);
+      setDogePrice(updatedDogePrice.data.updateDoge.price);
+    } catch (err) {
+      console.log("error updating dogePrice");
+      console.log(err);
+    }
+  };
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>Dogecoin price predictor</h1>
+        <p>One click = 10 cents</p>
+        <h2>$ {dogePrice.toFixed(2)}</h2>
+        <button onClick={updateDogePrice}>Doge</button>
       </header>
     </div>
   );
